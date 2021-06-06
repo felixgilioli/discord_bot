5b50e16ecffae60a6d999767d48ec591a8ba006d
Felix.Gilioli <felix.gilioli@senior.com.br>
2021-06-06 18:56:45 -0300

    description

--- src/main/kotlin/br/com/felixgilioli/discordbot/utils/environmentReader.kt
+++ src/main/kotlin/br/com/felixgilioli/discordbot/utils/environmentReader.kt
@@ -1,7 +1,7 @@
 package br.com.felixgilioli.discordbot.utils
 
 fun getEnv(envName: String) = System.getenv(envName)
-    ?: throw RuntimeException("could not find environment variable: $envName")
+    ?: throw RuntimeException("Could not find environment variable: $envName")
 
 fun getEnv(envName: String, defaultValue: () -> String) : String {
     return System.getenv(envName) ?: defaultValue.invoke()
