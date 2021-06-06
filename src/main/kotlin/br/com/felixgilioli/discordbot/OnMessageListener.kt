aa6f1c6443422344fe83b52fe51965f04ced639f
Felix.Gilioli <felix.gilioli@senior.com.br>
2021-06-06 19:06:23 -0300

    invert if condition

--- src/main/kotlin/br/com/felixgilioli/discordbot/OnMessageListener.kt
+++ src/main/kotlin/br/com/felixgilioli/discordbot/OnMessageListener.kt
@@ -8,21 +8,21 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter
 class OnMessageListener : ListenerAdapter() {
 
     override fun onMessageReceived(event: MessageReceivedEvent) {
-        if (event.message.contentRaw.startsWith(BOT_PREFIX)) {
-            var message = ""
+        if (!event.message.contentRaw.startsWith(BOT_PREFIX)) return
 
-            if (event.message.contentRaw.length > BOT_PREFIX.length) {
-                message = event.message.contentRaw.substring(BOT_PREFIX.length + 1)
-            }
+        var message = ""
 
-            println("Received the message: $message")
-
-            val response = WatsonAPI().getResponse(message)
-
-            println("Sending reply: $response")
-
-            this.sendMessage(event, response)
+        if (event.message.contentRaw.length > BOT_PREFIX.length) {
+            message = event.message.contentRaw.substring(BOT_PREFIX.length + 1)
         }
+
+        println("Received the message: $message")
+
+        val response = WatsonAPI().getResponse(message)
+
+        println("Sending reply: $response")
+
+        this.sendMessage(event, response)
     }
 
     private fun sendMessage(event: MessageReceivedEvent, message: String) {
